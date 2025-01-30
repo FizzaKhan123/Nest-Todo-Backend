99abee7111679f1ad841d969c98fa880ba9a1bc2
Fizzakhan123 <Fizzakhan2342@gmail.com>
2025-01-30 19:20:42 +0500

    entity path fixation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,26 @@
 import { Module } from '@nestjs/common';
 // import { MongooseModule } from '@nestjs/mongoose';
-import { User } from './entities/user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { User } from './entities/user.entity';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 import {AuthModule} from './auth/auth.module';
 import { TodosModule } from './todos/todos.module';
-import * as dotenv from 'dotenv';
-import { JwtModule } from '@nestjs/jwt';
+// import { JwtModule } from '@nestjs/jwt';
 import {JwtGlobalModule} from './JwtGlobalModule'
-import { Task } from './entities/todos.entity';
-import * as path from 'path';
-import * as glob from 'glob';
+// import { Task } from './entities/todos.entity';
+// import * as path from 'path';
+// import * as glob from 'glob';
 import { PostgressDatabaseProviderModule } from './providers/database/postgress/providers.module';
 import { AllSeeder } from './database/seeders/all-seeders';
 import { SeederModule } from './database/seeders/seeder.module';
-const fs = require('fs');
-dotenv.config();
-
-
-
+import { ConfigModule } from '@nestjs/config';
 
 
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     PostgressDatabaseProviderModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || "fd18b8832e225bc0f69432a14ad9c20c3dbbccf758decac5d77553e8743fc470",
-      signOptions: { expiresIn: '1h' },
-    }),
-    
     AuthModule,
     JwtGlobalModule,
     TodosModule,
--- src/providers/database/postgress/providers.module.ts
+++ src/providers/database/postgress/providers.module.ts
@@ -1,43 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
-import { readFileSync, readdirSync, statSync } from 'fs';
+
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: async () => ({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: '12345',
-        database: 'typeorm-todo',
-        entities: await getEntitiesFromPath(join(__dirname, '../../../entities')),
-        synchronize: false, 
-      }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'typeorm-todo',
+      entities:  [join(__dirname, '..', '..', '..', 'entities', '**', '*.entity.{ts,js}')],
+      synchronize: false, 
     }),
   ],
 })
 export class PostgressDatabaseProviderModule {}
-
-async function getEntitiesFromPath(entitiesPath: string): Promise<any[]> {
-  const entities: any[] = [];
-
-  const files = readdirSync(entitiesPath);
-    console.log("files",files);
-  for (const file of files) {
-    const filePath = join(entitiesPath, file);
-    const stats = statSync(filePath);
-
-    if (stats.isFile() && 
-        (filePath.endsWith('.entity.ts') || filePath.endsWith('.entity.js'))) {
-      const entityModule = require(filePath); 
-      entities.push(entityModule.default || entityModule); 
-    } else if (stats.isDirectory()) {
-      entities.push(...await getEntitiesFromPath(filePath)); 
-    }
-  }
-
-  return entities;
-}
